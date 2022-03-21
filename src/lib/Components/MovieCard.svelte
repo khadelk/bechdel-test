fd218710a19211e06382938a1adfb31b8ffa74a9
Elkeurti <khadidjaelkeurti@gmail.com>
2022-03-20 21:03:47 -0500

    Create Movie Card

---
+++ src/lib/Components/MovieCard.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let movie;
+</script>
+
+<div class="movie-card">
+	<a href={"/movie/"+movie.imdbid}> 
+		<img src={"https://image.tmdb.org/t/p/w400" + movie.poster_path} alt="{movie.title}">
+	</a>
+</div>
+
+<style>
+	.movie-card {
+
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script context="module">
 	export async function load({fetch, params}) {
 		const res = await fetch(
-			`https://api.themoviedb.org/3/movie/popular?api_key=12b43b9b8cd57e50b2441beea35b44b8`
+		`https://api.themoviedb.org/3/movie/popular?api_key=12b43b9b8cd57e50b2441beea35b44b8`
 		);
 		const popular = await res.json();
 		if(res.ok) {
@@ -13,42 +13,53 @@
 </script>
 
 <script>
+	import { onMount } from 'svelte';
 	import { bechdelData } from '$lib/stores.js'
 	import Header from '$lib/Components/Header.svelte'
 	import Search from '$lib/Components/Search.svelte'
-import { get } from 'svelte/store';
+	import MovieCard from '$lib/Components/MovieCard.svelte'
 	
 	export let searchTerm;
-	export let popular;
-	// console.log(popular.results)
-
-	let filteredBechdelData = [];
-	let smallBechdelData = $bechdelData.slice(0,100);
 	
-	$: {
-		if (searchTerm) {
-			filteredBechdelData = $bechdelData.filter(data => data.title.includes(searchTerm))
-		} else {
-			filteredBechdelData = smallBechdelData;
-		}
-	}
-
 	// Taken from https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
 	function getRandom(arr, n) {
 		var result = new Array(n),
-			len = arr.length,
-			taken = new Array(len);
+		len = arr.length,
+		taken = new Array(len);
 		if (n > len)
-			throw new RangeError("getRandom: more elements taken than available");
+		throw new RangeError("getRandom: more elements taken than available");
 		while (n--) {
 			var x = Math.floor(Math.random() * len);
 			result[n] = arr[x in taken ? taken[x] : x];
 			taken[x] = --len in taken ? taken[len] : len;
 		}
-    return result;
-}
-	// let displayMovies = (getRandom($bechdelData, 20))
-	// console.log(displayMovies)
+		return result;
+	}	
+
+	let movieData = []
+	//  = (getRandom($bechdelData, 20))
+	let displayMovies = []
+
+	$: {
+		if (searchTerm) {
+			movieData = $bechdelData.filter(data => data.title.includes(searchTerm))
+		} else {
+			movieData = (getRandom($bechdelData, 20))
+		}
+	}
+
+	$: {
+		movieData.forEach(movie => {
+			onMount(async() => {
+				const res = await fetch(`https://api.themoviedb.org/3/movie/tt${movie.imdbid}?api_key=12b43b9b8cd57e50b2441beea35b44b8`);
+				const json = await res.json();
+				displayMovies = [...displayMovies, json]
+			})
+		})
+	}
+
+	$: console.log(movieData)
+	$: console.log(displayMovies)
 	
 </script>
 
@@ -56,12 +67,16 @@ import { get } from 'svelte/store';
 
 <Search />
 
-<!-- {#each displayMovies as movie} -->
-<!-- <div>
-	<a href={"/movie/"+movie.imdbid}> {movie.title} </a>
-</div> -->
-	
+<div class="movies">
+	{#each displayMovies as movie}
+		<MovieCard {movie} />
+	{/each}
+</div>
 
-<!-- {/each} -->
 
-<!-- <RecentMovies {data} /> -->
+<style>
+	.movies {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+	}
+</style>
